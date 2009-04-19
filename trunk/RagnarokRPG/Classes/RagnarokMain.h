1d6aa6adec228b3f1f247eb80870a23059f13fb2
jeremypjoseph <jeremypjoseph@8b492eb8-268e-11de-a509-e7584d489398>
2009-04-19 18:44:49 +0000

    added missing classes

---
+++ trunk/RagnarokRPG/Classes/RagnarokMain.h
@@ -0,0 +1,18 @@
+//
+//  RagnarokMain.h
+//  RagnarokRPG
+//
+//  Created by Jeremy Joseph on 4/17/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface RagnarokMain : NSObject {
+
+}
+
+
+
+@end
