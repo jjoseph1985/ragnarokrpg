19139432c18d657e1f78f9635774e52d5c9dbe5d
jeremypjoseph <jeremypjoseph@8b492eb8-268e-11de-a509-e7584d489398>
2009-04-22 02:43:28 +0000

    added missing classes

---
+++ RagnarokRPG/Classes/EnemyCharacter.m
@@ -0,0 +1,78 @@
+//
+//  EnemyCharacter.m
+//  RagnarokRPG
+//
+//  Created by Beth DeCarlo on 4/11/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "EnemyCharacter.h"
+
+
+@implementation EnemyCharacter
+
+- (void) initVariables
+{
+	hitpoints = 25;
+	mana = 12;
+	attackpower = 5;
+	defense = 2;
+	magicpower = 5;
+}
+
+- (void) subtractDamageFromHP:(int) damage
+{
+	hitpoints -= damage;
+}
+
+- (void) setHitpoints: (int) hp
+{
+	hitpoints = hp;
+}
+
+- (void) setMana: (int) m
+{
+	mana = m;
+}
+
+- (void) setAttackPower: (int) ap
+{
+	attackpower = ap;
+}
+
+- (void) setDefense: (int) d
+{
+	defense = d;
+}
+
+- (void) setMagicPower: (int) mp
+{
+	magicpower = mp;
+}
+
+- (int) getHitpoints
+{
+	return hitpoints;
+}
+
+- (int) getMana
+{
+	return mana;
+}
+
+- (int) getAttackPower
+{
+	return attackpower;
+}
+
+- (int) getDefense
+{
+	return defense;
+}
+
+- (int) getMagicPower
+{
+	return magicpower;
+}
+
+@end
